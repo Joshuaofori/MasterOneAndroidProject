d144c773a5f8f1fabfb193670f6f3aea7a5c0ae7
Joshua Ofori <oforijoshua37@gmail.com>
2021-02-18 10:06:31 +0100

    second on this branch

--- app/src/main/java/com/example/android/jobfinder/MainActivity.java
+++ app/src/main/java/com/example/android/jobfinder/MainActivity.java
@@ -7,13 +7,22 @@ import android.os.Bundle;
 
 import com.example.android.jobfinder.User.JobFinderDatabase;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity {
+
+    Executor executor = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        JobFinderDatabase db = Room.databaseBuilder(getApplicationContext(),
-                JobFinderDatabase.class, "database-name").build();
+        //executor.execute(()->{
+            JobFinderDatabase db = Room.databaseBuilder(getApplicationContext(),
+                    JobFinderDatabase.class, "database-name").build();
+        //});
+
+
     }
 }
--- app/src/main/java/com/example/android/jobfinder/User/JobFinderDatabase.java
+++ app/src/main/java/com/example/android/jobfinder/User/JobFinderDatabase.java
@@ -3,11 +3,10 @@ package com.example.android.jobfinder.User;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+@Database(entities = {User.class , Job.class}, version = 1)
 public abstract class JobFinderDatabase extends RoomDatabase {
-    @Database(entities = {User.class , Job.class}, version = 1)
 
-    public abstract class AppDatabase extends RoomDatabase {
         public abstract UserDao userDao();
         public abstract JobDao jobDao();
     }
-}
+
